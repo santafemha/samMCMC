c21c0b7cc9ab07e04c9719583bd2078e669f8e6b
ajrominger <ajrominger@gmail.com>
2019-05-09 13:57:28 -0600

    added a print method for `sam` objects

---
+++ R/sam_methods.R
@@ -0,0 +1,15 @@
+#' @title Print a \code{sam} object
+#'
+#' @description Place holder
+#'
+#'
+#' @param x The \code{sam} object to print
+#'
+#' @return Silently returns the input \code{chain} object
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#' @export
+
+print.sam <- function(x) {
+    cat('this is a `sam` object')
+}
